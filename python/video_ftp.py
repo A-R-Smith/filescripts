d5308cd2ae96496ebbb126741440ef7b625d5b7c
smita16 <alex.smith@nationwide.com>
2018-01-02 15:13:40 -0500

    ftp stuff

---
+++ python/video_ftp.py
@@ -0,0 +1,24 @@
+import ftplib
+import configparser
+
+#setup URL and username
+ftpURL = "nwdropbox.centralus.cloudapp.azure.com"
+username = "iotftp"
+
+#proxy info (only needed if running on enterprise network)
+# session = ftplib.FTP("localhost:3128","smita16","footba74")
+session = ftplib.FTP()
+session.connect("localhost",3128)
+
+
+print("made it here")
+#read config file containing password
+configs = configparser.ConfigParser()
+configs.read("C:/ws/filescripts/config.ini")
+passwd = destination_dir = configs.get("Passwords", "iotftp")
+session = ftplib.FTP(ftpURL,username,passwd)
+
+#start session
+msg = session.login(username+"@"+ftpURL,passwd)
+
+print(msg)
