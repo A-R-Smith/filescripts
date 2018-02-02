ee38482331fabf45d064530af818f4bc02a21ec4
smita16 <alex.smith@nationwide.com>
2018-02-02 16:11:03 -0500

    added rm-trail.py

--- sh/backup.sh
+++ sh/backup.sh
@@ -2,14 +2,14 @@
 sudo azcopy \
         --source https://nwford.file.core.windows.net/ford/prod/ \
         --destination https://nwford.file.core.windows.net/fordbck/ \
-        --source-key QJyO9oM3YVahmJ1zKkEPEABTq3W5MMWYGOoyRHDoFdb42FSVVInawPmJbO$
-        --dest-key QJyO9oM3YVahmJ1zKkEPEABTq3W5MMWYGOoyRHDoFdb42FSVVInawPmJbOqg$
+        --source-key QJyO9oM3YVahmJ1zKkEPEABTq3W5MMWYGOoyRHDoFdb42FSVVInawPmJbOqgO+/vVCu5Rxdjj2ka4+dO7DlHbw== \
+        --dest-key QJyO9oM3YVahmJ1zKkEPEABTq3W5MMWYGOoyRHDoFdb42FSVVInawPmJbOqgO+/vVCu5Rxdjj2ka4+dO7DlHbw== \
         --recursive \
         --exclude-older --exclude-newer
 sudo azcopy \
         --source https://nwford.file.core.windows.net/ford/prod/ \
-        --destination https://nwford.blob.core.windows.net/nwfordhd-2017-08-28t$
-        --source-key QJyO9oM3YVahmJ1zKkEPEABTq3W5MMWYGOoyRHDoFdb42FSVVInawPmJbO$
-        --dest-key QJyO9oM3YVahmJ1zKkEPEABTq3W5MMWYGOoyRHDoFdb42FSVVInawPmJbOqg$
+        --destination https://nwford.blob.core.windows.net/nwfordhd-2017-08-28t14-42-20-095z/ford/ \
+        --source-key QJyO9oM3YVahmJ1zKkEPEABTq3W5MMWYGOoyRHDoFdb42FSVVInawPmJbOqgO+/vVCu5Rxdjj2ka4+dO7DlHbw== \
+        --dest-key QJyO9oM3YVahmJ1zKkEPEABTq3W5MMWYGOoyRHDoFdb42FSVVInawPmJbOqgO+/vVCu5Rxdjj2ka4+dO7DlHbw== \
         --recursive \
         --exclude-older --exclude-newer
